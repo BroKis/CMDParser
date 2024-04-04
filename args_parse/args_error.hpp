971bf5e9729eecb4dbacb136a781459f35a4508e
Ilya_Emelyanenko <ilakislev43@gmail.com>
2024-04-04 15:10:51 +0300

    2.0_1

--- args_parse/args_error.hpp
+++ args_parse/args_error.hpp
@@ -3,22 +3,44 @@
 #include <optional>
 namespace args_error
 {
+	/**
+	* @brief Структура для создания ошибки
+	*/
 	struct Error {
-		std::string description;
+		std::string description;//<поле для описания ошибки
 	};
 
+	/**
+	* @brief Класс для реализации выбрасывания ошибок
+	* Данный класс реализует методы для реализации механизма ошибок
+	*/
 	class [[nodiscard]] ParseResult {
-		std::optional<Error> error;
+		std::optional<Error> error;///<Поле содержащее структуру @Error
 
-		ParseResult() = default;
-		ParseResult(Error error) : error{ std::move(error) } {};
+		ParseResult() = default; ///<Конструктор по умолчанию
+		ParseResult(Error error) : error{ std::move(error) } {}; ///<Конструктор, если есть ошибка
 
 	public:
+		/**
+		*@brief Метод который возвращает конструктор по умолчанию, если нет ошибок
+		*/
 		static ParseResult Ok() { return ParseResult{}; };
-		static ParseResult Fail(Error error) { return ParseResult{ std::move(error) }; };
+		/**
+		*@brief Метод который возвращает конструктор с ошибкой, если ошибка есть
+		*/
+		static ParseResult Fail(
+			Error error///<Поле для описания ошибки
+		) { return ParseResult{ std::move(error) }; };
 
+		/**
+		*@brief Метод который возвращает истинное значение, если ошибки нет
+		*/
 		[[nodiscard]]bool isOk() const { return !this->error.has_value(); };
 
+
+		/**
+		*@brief Метод который возвращает ложное значение, если ошибка есть
+		*/
 		[[nodiscard]] const Error& Error() const { return this->error.value(); };
 	};
 }
--- args_parse/args_validator.hpp
+++ args_parse/args_validator.hpp
@@ -3,70 +3,126 @@
 #include "args_error.hpp"
 namespace args_validator
 {
+	/**
+	* @brief Базовый класс-валидатор для валидирования целых чисел
+	*/
 	class IntValidator
 	{
 	public:
 		virtual ~IntValidator();
+		/**
+		* @brief Виртуальный метод для проверки целых чисел
+		*/
 		[[discard]]
-		virtual args_error::ParseResult CheckInt(const std::string& fresh_parameter) = 0;
+		virtual args_error::ParseResult CheckInt(
+			const std::string& fresh_parameter///<Валидируемая строка
+		) = 0;
 	};
+	/**
+	* @brief Класс-валидатор наследник для валидирования целых чисел в диапазоне
+	*/
 	class IntInRangeValidator :public IntValidator
 	{
 		~IntInRangeValidator() {};
+		/**
+		* @brief Виртуальный метод для проверки целых чисел в диапазоне
+		*/
 		[[discard]]
 		args_error::ParseResult CheckInt(const std::string& fresh_parameter) override;
 	};
 
+	/**
+	* @brief Базовый класс-валидатор для валидирования строки
+	*/
 	class StringValidator
 	{
 	public:
 		virtual ~StringValidator();
+		/**
+		* @brief Виртуальный метод для проверки строк
+		*/
 		[[discard]]
 		virtual args_error::ParseResult CheckString(const std::string& fresh_parameter) = 0;
 
 	};
 
+	/**
+	* @brief Класс-валидатор наследник для валидирования строки
+	*/
 	class NormalStringValidator : public StringValidator
 	{
 	public:
 		[[discard]]
+		/**
+		* @brief Метод для проверки строк в нормальной форме без специальных символов
+		*/
 		args_error::ParseResult CheckString(const std::string& fresh_parameter) override;
 	};
 
+	/**
+	* @brief Класс-валидатор наследник для валидирования названия текстовых файлов
+	*/
 	class FileStringValidator :public StringValidator
 	{
 	public:
+		/**
+		* @brief Метод для проверки строк на соответствие формату текстовых файлов
+		*/
 		[[discard]]
 		args_error::ParseResult CheckString(const std::string& fresh_parameter) override;
 	};
-
+	/**
+	* @brief Базовый класс-валидатор для валидирования булевых значений
+	*/
 	class BoolValidator
 	{
 	public:
 		virtual ~BoolValidator();
+		/**
+		* @brief Виртуальный метод для проверки строк на соответствие значению истины/ложь
+		*/
 		[[discard]]
 		virtual args_error::ParseResult CheckBool(const std::string& fresh_parameter) = 0;
 	};
 
+	/**
+	* @brief Класс-валидатор для валидирования булевых значений в нормальной форме
+	*/
 	class NormalBoolValidator : public BoolValidator
 	{
 	public:
+		/**
+		* @brief Метод для проверки строк на соответствие значению истины/ложь
+		*/
 		[[discard]]
 		args_error::ParseResult CheckBool(const std::string& fresh_parameter) override;
 
 	};
 
+
+	/**
+	* @brief Базовый класс-валидатор для валидирования поступающих аргументов для парсинга
+	*/
 	class ParserValidator
 	{
 	public:
 		virtual ~ParserValidator();
+		/**
+		* @brief Метод для проверки строк на соответствие аргументов для парсинга 
+		*/
 		[[discard]]
 		virtual args_error::ParseResult CheckInput(const std::string& fresh_parameter) = 0;
 	};
-
+	/**
+	* @brief Класс-валидатор для валидирования поступающих аргументов для парсинга
+	*/
 	class NormalParserValidator : public ParserValidator
 	{
 	public:
+		/**
+		* @brief Метод для проверки строк на соответствие аргументов для парсинга
+		*/
+		[[discard]]
 		args_error::ParseResult CheckInput(const std::string& fresh_parameter) override;
 	};
 
